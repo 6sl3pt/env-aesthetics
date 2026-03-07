d271dfd45f6cf2aab6b032022c8efc1d8da7e761
6sl3pt <eochannelformal@gmail.com>
2026-03-07 15:01:58 +0700

    refactor: convert app config to dotfiles

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -1,4 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.termguicolors = true
+
 vim.opt.relativenumber = false
+vim.opt.wrap = true
+
+vim.g.lazyvim_prettier_needs_config = false
--- dotfiles/nvim/lua/plugins/theme.lua
+++ dotfiles/nvim/lua/plugins/theme.lua
@@ -1,7 +1,11 @@
 return {
   {
     "ribru17/bamboo.nvim",
+    lazy = false,
     priority = 1000,
+    config = function()
+      require("bamboo").load()
+    end,
   },
   {
     "LazyVim/LazyVim",
