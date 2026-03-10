410179876ffcb1de0e80a76a9e171b81beb49b7a
6sl3pt <eochannelformal@gmail.com>
2026-03-10 12:41:28 +0700

    feat: change treesitter theme

--- dotfiles/nvim/lua/plugins/theme.lua
+++ dotfiles/nvim/lua/plugins/theme.lua
@@ -5,9 +5,10 @@ return {
     priority = 1000,
     config = function()
       require("bamboo").load()
-      vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = "#88af6c" })
-      vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = "#88af6c" })
+      vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = "#63b07a" })
+      vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = "#63b07a" })
       vim.api.nvim_set_hl(0, "NeoTreeRootName", { fg = "#c1c497", bold = true, italic = true })
+      vim.api.nvim_set_hl(0, "NeoTreeDotfile", { fg = "#53685b" })
     end,
   },
   {
